80329ac59b141550e47a2854fe23e5fba96bc005
Hai Duy Huynh <huynhhaiduy17051999@gmail.com>
2024-02-24 03:46:22 +0100

    event

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,11 +7,22 @@ class MyComponent extends React.Component {
     address: "Saigon",
   };
 
+  handleClick(event) {
+    // console.log(">> click my button");
+    console.log("My name is ", this.state.name);
+  }
+
+  handleOnMouseOver(event) {
+    console.log(event.pageX);
+  }
+
   // JSX
   render() {
     return (
       <div>
         My name is {this.state.name} and I am from {this.state.address}
+        <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
+        <button onClick={this.handleClick}>Click me</button>
       </div>
     );
   }
