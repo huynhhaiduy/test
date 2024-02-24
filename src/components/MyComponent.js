33c823cc44bddec46d17d1796c63a70f06eef9a3
Hai Duy Huynh <huynhhaiduy17051999@gmail.com>
2024-02-24 19:21:57 +0100

    Nesting Component

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,48 +1,12 @@
 import React from "react";
+import UserInfor from "./UserInfor";
 
 class MyComponent extends React.Component {
-  state = {
-    name: "Duy",
-    age: 25,
-    address: "Saigon",
-  };
-
-  handleClick(event) {
-    // console.log(">> click my button");
-    console.log("My name is ", this.state.name);
-
-    this.setState({
-      name: "Hai Duy",
-    });
-  }
-
-  handleOnMouseOver(event) {
-    console.log(event.pageX);
-  }
-
-  handleOnChangeInput = (event) => {
-    this.setState({
-      name: event.target.value,
-    });
-  };
-
-  handleOnSubmit = (event) => {
-    event.preventDefault();
-    console.log(this.state);
-  };
-
   // JSX
   render() {
     return (
       <div>
-        My name is {this.state.name} and I am from {this.state.address}
-        <form onSubmit={(event) => this.handleOnSubmit(event)}>
-          <input
-            type="text"
-            onChange={(event) => this.handleOnChangeInput(event)}
-          />
-          <button>Submit</button>
-        </form>
+        <UserInfor />
       </div>
     );
   }
