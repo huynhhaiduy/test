6cb6253169df0cb48ddc9cf3754c85341e026ae7
Hai Duy Huynh <huynhhaiduy17051999@gmail.com>
2024-03-01 20:41:46 +0100

    Design New Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -26,11 +26,13 @@ const Header = () => {
             </NavLink>
           </Nav>
           <Nav>
-            <NavDropdown title="Setting" id="basic-nav-dropdown">
+            <button className="btn-login">Login</button>
+            <button className="btn-signup">Sign up</button>
+            {/* <NavDropdown title="Setting" id="basic-nav-dropdown">
               <NavDropdown.Item>Login</NavDropdown.Item>
               <NavDropdown.Item>Logout</NavDropdown.Item>
               <NavDropdown.Item>Profile</NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
