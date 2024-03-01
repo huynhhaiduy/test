99356c0a23130ee5c4307112a7e7304fa588958f
Hai Duy Huynh <huynhhaiduy17051999@gmail.com>
2024-03-01 03:55:29 +0100

    Điều Hướng Trang với Links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app-container">
       <Header />
-      <button>test</button>
+      <div>
+        test link
+        <div>
+          <button>
+            <Link to="/users">Go to User page</Link>
+          </button>
+          <button>
+            <Link to="/admins">Go to Admin page</Link>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
