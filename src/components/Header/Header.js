72572a2d0c5576ca57f0b524e561b6d3de51edea
Hai Duy Huynh <huynhhaiduy17051999@gmail.com>
2024-03-04 08:09:20 +0100

    Sử Dụng Redux Lưu Thông Tin Người Dùng

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,12 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { NavLink, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
+  const account = useSelector((state) => state.user.account);
+
   const navigate = useNavigate();
 
   const handleLogin = () => {
@@ -36,17 +40,21 @@ const Header = () => {
             </NavLink>
           </Nav>
           <Nav>
-            <button className="btn-login" onClick={() => handleLogin()}>
-              Login
-            </button>
-            <button className="btn-signup" onClick={() => handleRegister()}>
-              Sign up
-            </button>
-            {/* <NavDropdown title="Setting" id="basic-nav-dropdown">
-              <NavDropdown.Item>Login</NavDropdown.Item>
-              <NavDropdown.Item>Logout</NavDropdown.Item>
-              <NavDropdown.Item>Profile</NavDropdown.Item>
-            </NavDropdown> */}
+            {isAuthenticated === false ? (
+              <>
+                <button className="btn-login" onClick={() => handleLogin()}>
+                  Login
+                </button>
+                <button className="btn-signup" onClick={() => handleRegister()}>
+                  Sign up
+                </button>
+              </>
+            ) : (
+              <NavDropdown title="Setting" id="basic-nav-dropdown">
+                <NavDropdown.Item>Logout</NavDropdown.Item>
+                <NavDropdown.Item>Profile</NavDropdown.Item>
+              </NavDropdown>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
