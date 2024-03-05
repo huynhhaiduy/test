f55abe3ebb277365239f5610b47caf9065e424e4
Hai Duy Huynh <huynhhaiduy17051999@gmail.com>
2024-03-05 05:04:11 +0100

    Design Danh Sách Bài Thi Của User – Display List Quiz

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -51,6 +51,10 @@ const postRegister = (email, password, username) => {
   return axios.post(`/api/v1/register`, { email, password, username });
 };
 
+const getQuizByUser = () => {
+  return axios.get("/api/v1/quiz-by-participant");
+};
+
 export {
   postCreateNewUser,
   getAllUsers,
@@ -59,4 +63,5 @@ export {
   getUserWithPaginate,
   postLogin,
   postRegister,
+  getQuizByUser,
 };
