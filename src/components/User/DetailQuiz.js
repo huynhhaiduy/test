087d6ec0895d21d19f48f7cdaad72c1b6fce6930
Hai Duy Huynh <huynhhaiduy17051999@gmail.com>
2024-03-05 09:31:43 +0100

    Design Question – Tạo Giao Diện Hiển Thị Question

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,14 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams, useLocation } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiService";
 import _ from "lodash";
 import "./DetailQuiz.scss";
+import Question from "./Question";
 
 const DetailQuiz = (props) => {
   const params = useParams();
   const location = useLocation();
 
   const quizId = params.id;
+
+  const [dataQuiz, setDataQuiz] = useState([]);
+  const [index, setIndex] = useState(0);
 
   useEffect(() => {
     fetchQuestions();
@@ -37,7 +41,19 @@ const DetailQuiz = (props) => {
           return { questionId: key, answers, questionDescription, image };
         })
         .value();
-      console.log(data);
+      //   console.log(data);
+      setDataQuiz(data);
+    }
+  };
+
+  const handlePrev = () => {
+    if (index - 1 < 0) return;
+    setIndex(index - 1);
+  };
+
+  const handleNext = () => {
+    if (dataQuiz && dataQuiz.length > index + 1) {
+      setIndex(index + 1);
     }
   };
 
@@ -55,18 +71,19 @@ const DetailQuiz = (props) => {
         </div>
 
         <div className="q-content">
-          <div className="question">Question 1: How are you doing?</div>
-
-          <div className="answer">
-            <div className="a-child">A. abc</div>
-            <div className="a-child">B. abc</div>
-            <div className="a-child">C. abc</div>
-          </div>
+          <Question
+            index={index}
+            data={dataQuiz && dataQuiz.length > 0 ? dataQuiz[index] : []}
+          />
         </div>
 
         <div className="footer">
-          <button className="btn btn-secondary">Prev</button>
-          <button className="btn btn-primary">Next</button>
+          <button className="btn btn-secondary" onClick={() => handlePrev()}>
+            Prev
+          </button>
+          <button className="btn btn-primary" onClick={() => handleNext()}>
+            Next
+          </button>
         </div>
       </div>
 
