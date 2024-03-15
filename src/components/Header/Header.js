88de289781dd315361229aa72331b402fc556567
Hai Duy Huynh <huynhhaiduy17051999@gmail.com>
2024-03-15 17:50:31 +0700

    Log out

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,11 +3,16 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { NavLink, useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../services/apiService";
+import { toast } from "react-toastify";
+import { doLogout } from "../../redux/action/userAction";
 
 const Header = () => {
   const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
   const account = useSelector((state) => state.user.account);
+
+  const dispatch = useDispatch();
 
   const navigate = useNavigate();
 
@@ -17,6 +22,18 @@ const Header = () => {
 
   const handleRegister = () => {
     navigate("/register");
+  };
+
+  const handleLogOut = async () => {
+    let rs = await logout(account.email, account.refresh_token);
+    if (rs && rs.EC === 0) {
+      // clear data redux
+      dispatch(doLogout());
+
+      navigate("/login");
+    } else {
+      toast.error(rs.EM);
+    }
   };
 
   return (
@@ -51,8 +68,10 @@ const Header = () => {
               </>
             ) : (
               <NavDropdown title="Setting" id="basic-nav-dropdown">
-                <NavDropdown.Item>Logout</NavDropdown.Item>
                 <NavDropdown.Item>Profile</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => handleLogOut()}>
+                  Logout
+                </NavDropdown.Item>
               </NavDropdown>
             )}
           </Nav>
