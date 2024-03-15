8fa8df65986921947fe1328e6e00e1bcd1c9ca13
Hai Duy Huynh <huynhhaiduy17051999@gmail.com>
2024-03-16 01:34:33 +0700

    Design Dashboard

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -44,6 +44,8 @@ const DetailQuiz = (props) => {
             item.answers.isSelected = false;
             answers.push(item.answers);
           });
+          answers = _.orderBy(answers, ["id"], ["asc"]);
+
           return { questionId: key, answers, questionDescription, image };
         })
         .value();
